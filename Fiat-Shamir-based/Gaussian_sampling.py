331046f66e950a27da47f6269df480d32a8e8349
Krishna Acharya <f2015675@goa.bits-pilani.ac.in>
2017-06-16 19:54:19 +0530

    resolves precomputation and Bernoulli_cosh(x) in Gaussian_sampling.py

--- Fiat-Shamir-based/Gaussian_sampling.py
+++ Fiat-Shamir-based/Gaussian_sampling.py
@@ -1,13 +1,24 @@
+'''
+Transcedental function Bernoulli sampling, Gaussian sampling
+used for embedded devices where floating point computations
+are not native and memory is limited.
+'''
 from math import exp
 import random
-# to do statiscal tests
+
 def Bernoulli_rv(c):
 	'''
 		c is a parameter of Bernoulli distribution
 	'''
 	return 1 if random.random() < c else 0
 
-def Bernoulli_exp(x, f):
+
+# Precomputation for Bernoulli_exp(x)
+f = 3 # fixed real 
+l = 64 # max number of bits in positive integer x
+c = [exp(-float(2**i) / f) for i in range(0, l)] # Ci stores exp(-2^i / f)
+
+def Bernoulli_exp(x):
 	'''
 	Description:
 	Algorithm 8 in BLISS paper
@@ -21,55 +32,31 @@ def Bernoulli_exp(x, f):
 	f: float
 	'''
 	bin_rep = map(int, list(bin(x)[2:])) # list with 0's and 1's reprsenting x. msb is first as usual
-	l = len(bin_rep) 
-	# Precomputation requires only log(x) base 2 number of elements  
-	c = [exp(-float(2**i) / f) for i in range(0, l)] # Ci stores exp(-2^i / f)
+	d = len(bin_rep) # length of the current integer in binary, d < l
 
 	# starting from l-1, as then smallest probabilities are checked first and algorithm terminates faster 
-	for i in range(0, l):
+	for i in range(0, d):
 		if(bin_rep[i]):
-			A = Bernoulli_rv(c[l-i-1])
+			A = Bernoulli_rv(c[d-i-1])
 			if not A:
 				return 0
 	return 1
 
-def Bernoulli_cosh(x, f):
+# uses the same fixed real f
+def Bernoulli_cosh(x):
 	'''
 	Sample according to 1/cosh(x/f)
 	Extends corollary 6.4 from BLISS paper
 	'''
 	powx = abs(x)
 	while(True):
-		A = Bernoulli_exp(powx, f) # each iteration this changes as randomness comes from Bernoulli_exp exp(-|x|/f)
+		A = Bernoulli_exp(powx) # each iteration this changes as randomness comes from Bernoulli_exp exp(-|x|/f)
 		if(A):
 			return 1
-		B = Bernoulli_rv(0.5)
-		if not(B or A):			# todo: check whether it is OR with the same Bexp or diff
+		B = Bernoulli_rv(0.5) or Bernoulli_exp(powx)  # has to be seperate Bernoulli_exp(powx) call as we dont want dependence on A
+		if not(B):			
 			return 0
 
 def disc_Gaussian_sampling():
 	pass
-	
-'''
-def Bernoulli_frac_op(a, b):
-	
-	#Description:
-	#Algorithm 9 in BLISS paper
-	#This is just for validating lemma 6.3 in the paper
-	#Use it as a template
-
-	#Sampling Ba op Bb  which is the same as Ba/(1-(1-a)*b)  
-
-	#i/p: a and b which are the parameters for the Bernoulli distribution Ba and Bb
-	
-	while(True):
-		A = Bernoulli_rv(a)
-		if(A == 1):
-			return 1
-		B = Bernoulli_rv(b)
-		if(B == 0):
-			return 0
-'''				
-
-
 	
